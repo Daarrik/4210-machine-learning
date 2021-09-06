05d9972955a44464f6139d2d1ed7ebb36d2bd7dc
Darrik Houck <77186596+Daarrik@users.noreply.github.com>
2021-09-05 22:14:10 -0700

    Update find_s.py

--- find_s.py
+++ find_s.py
@@ -3,7 +3,7 @@
 # FILENAME: find_s.py
 # SPECIFICATION: Program for the Find S algorithm
 # FOR: CS 4200- Assignment #1
-# TIME SPENT: how long it took you to complete the assignment
+# TIME SPENT: 30 min
 #-----------------------------------------------------------*/
 
 #IMPORTANT NOTE: DO NOT USE ANY ADVANCED PYTHON LIBRARY TO COMPLETE THIS CODE SUCH AS numpy OR pandas. You have to work here only with standard vectors and arrays
